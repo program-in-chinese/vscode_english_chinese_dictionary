e00d74a82cc94cff89b484d9de742d60a82b419a
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-11-28 23:53:44 -0800

    package设置. 已运行increase-memory-limit

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,23 +1,21 @@
-/* global suite, test */
-
 //
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 
 // The module 'assert' provides assertion methods from node
-const assert = require('assert');
+import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-const vscode = require('vscode');
-const myExtension = require('../extension');
+import * as vscode from 'vscode';
+import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function() {
+suite("Extension Tests", () => {
 
     // Defines a Mocha unit test
-    test("Something 1", function() {
+    test("Something 1", () => {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
