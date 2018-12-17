697dcd29caaecf8864446db708842f702b3edefa
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-12-16 16:53:38 -0800

    #17 演示替换其中词汇

--- src/provider.ts
+++ src/provider.ts
@@ -5,46 +5,34 @@ import * as vscode from 'vscode';
 export default class Provider implements vscode.TextDocumentContentProvider{
 
 	static scheme = 'references';
-
-	private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
-	private _subscriptions: vscode.Disposable;
-
-	constructor() {
-	}
+	private _演示字典: Map<String, String> = new Map([
+		["BasicCalculator", "基本的计算器"],
+		["Calculator", "计算器类"],
+		["calculator", "计算器"],
+		["add", "加"],
+		["subtract", "減"],
+		["multiply", "乘"],
+		["divide", "除"],
+		["first", "第一"],
+		["second", "第二"],
+		["number", "数"],
+		["divisor", "被除数"]
+	]);
 
 	dispose() {
-		this._subscriptions.dispose();
-		this._onDidChange.dispose();
 	}
 
-	// Expose an event to signal changes of _virtual_ documents
-	// to the editor
-	get onDidChange() {
-		return this._onDidChange.event;
-	}
-
-	// Provider method that takes an uri of the `references`-scheme and
-	// resolves its content by (1) running the reference search command
-	// and (2) formatting the results
 	provideTextDocumentContent(uri: vscode.Uri): string | Thenable<string> {
-		return vscode.window.activeTextEditor.document.getText();
-		/*return "class BasicCalculator {\n"
-		+ "  public static void main(String[] args) {\n"
-		+ "  	 BasicCalculator calculator = new BasicCalculator();\n"
-		+ "    calculator.divide(1, 2);\n"
-		+ "  }\n"
-		+ "}";*/
-		// Decode target-uri and target-position from the provided uri and execute the
-		// `reference provider` command (http://code.visualstudio.com/docs/extensionAPI/vscode-api-commands).
-		// From the result create a references document which is in charge of loading,
-		// printing, and formatting references
-		const [target, pos] = decodeLocation(uri);
-		/*return vscode.commands.executeCommand<vscode.WorkspaceEdit>('vscode.executeDocumentRenameProvider', target, pos, "中文命名").then(edit => {
-			if (edit) {
-				return vscode.workspace.applyEdit(edit);
-			}
-		});*/
+		var 新内容 = vscode.window.activeTextEditor.document.getText();
+		for (var 原命名 of this._演示字典.keys()) {
+		  新内容 = this._replaceAll(新内容, 原命名, this._演示字典.get(原命名));
+		}
+		return 新内容;
 	}
+
+	private _replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+  }
 }
 
 let seq = 0;
@@ -52,9 +40,4 @@ let seq = 0;
 export function encodeLocation(uri: vscode.Uri, pos: vscode.Position): vscode.Uri {
 	const query = JSON.stringify([uri.toString(), pos.line, pos.character]);
 	return vscode.Uri.parse(`${Provider.scheme}:test.java?${query}#${seq++}`);
-}
-
-export function decodeLocation(uri: vscode.Uri): [vscode.Uri, vscode.Position] {
-	let [target, line, character] = <[string, number, number]>JSON.parse(uri.query);
-	return [vscode.Uri.parse(target), new vscode.Position(line, character)];
 }
